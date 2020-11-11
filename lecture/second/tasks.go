df782038dc998f6938819106e7f922b0ade40ae8
marinco <marin.milina96@gmail.com>
2020-11-11 14:55:59 +0100

    Add greatest common divisor function

--- lecture/second/tasks.go
+++ lecture/second/tasks.go
@@ -1,1 +1,11 @@
 package second
+
+func Gcd(b int, c int) []Step {
+	var steps = make([]Step, 0)
+	for c > 0 {
+		var step = Step{A: b / c, B: b, R: b % c}
+		steps = append(steps, step)
+		b, c = c, b%c
+	}
+	return steps
+}
