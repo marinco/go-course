48d70edc424e42b09bf73f8cd203dd804c38c345
Marin Milina <marin.milina@5x9networks.com>
2020-12-17 19:04:34 +0100

    Add main function

---
+++ lecture/lecture4/main.go
@@ -0,0 +1,16 @@
+package lecture4
+
+import (
+	"net/http"
+)
+
+func Main() {
+	//get("https://google.com")
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/post", postHandler)
+	http.HandleFunc("/file", fileHandler)
+	http.HandleFunc("/json", jsonHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
