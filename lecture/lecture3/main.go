ca7fe2367c73fb30e5fe549e41ca0e7c5dcaa168
Marin Milina <marin.milina@5x9networks.com>
2020-12-17 17:12:48 +0100

    Add locking function

--- lecture/lecture3/main.go
+++ lecture/lecture3/main.go
@@ -1,12 +1,32 @@
 package lecture3
 
 import (
+	"fmt"
 	"sync"
 )
 
 var messages chan string
 var myWaitGroup sync.WaitGroup
 
+var wg sync.WaitGroup
+var mux sync.Mutex
+var sum int
+
 func Main() {
 
+	wg.Add(10000)
+	var i int
+	for i = 0; i < 10000; i++ {
+		go foo(i)
+	}
+	wg.Wait()
+	fmt.Println(sum)
+}
+
+func foo(i int) {
+	mux.Lock()
+	defer mux.Unlock()
+	sum += 1
+	//mux.Unlock()
+	wg.Done()
 }
